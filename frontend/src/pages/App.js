7e0efe5c602c3b7c8f70ac2f0df722cef22f883d
alexandrsashin <alexandrsashin@gmail.com>
2019-11-15 22:41:53 +0300

    Pasted demo with images

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,10 +1,54 @@
 import React, { PureComponent } from "react";
 
 class App extends PureComponent {
+  canvas = React.createRef();
+  layerSlider = React.createRef();
+
+  state = {
+    layerValue: "1"
+  };
+
+  componentDidMount() {
+    this.loadImageToCanvas();
+  }
+
+  loadImageToCanvas = () => {
+    const { layerValue } = this.state;
+    const canvas = this.canvas.current;
+    var context = canvas.getContext("2d");
+
+    var imageObj = new Image();
+
+    var imageUrl =
+      "https://junction-planreview.azurewebsites.net/api/patients/Head_Neck/plans/JSu-IM101/RenderedBitmaps/";
+    imageObj.src = imageUrl + layerValue;
+
+    imageObj.onload = function() {
+      context.drawImage(this, 0, 0);
+    };
+  };
+
+  onLayerChange = ({ target: { value } }) => {
+    this.setState({ layerValue: value }, () => this.loadImageToCanvas());
+  };
+
   render() {
+    const { layerValue } = this.state;
     return (
       <div>
-        App
+        <div>
+          <canvas ref={this.canvas} width="600" height="600"></canvas>
+        </div>
+        <div>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            value={layerValue}
+            ref={this.layerSlider}
+            onChange={this.onLayerChange}
+          ></input>
+        </div>
       </div>
     );
   }
